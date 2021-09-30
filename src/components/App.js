17ee75e266b421859b409bc338d8d70fc4bf0431
denis_levchenko <tranzi90@gmail.com>
2021-09-30 17:34:41 +0300

    add final functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,34 @@ import { Container, Table } from "react-bootstrap"
 import CurrencyItem from "./CurrencyItem"
 import axios from "axios"
 import DropdownList from "./DropdownList"
+import CountryCurrs from '../currency.json'
 
 const App = () => {
-	const [currency, setCurrency] = useState({})
+	const [currency, setCurrency] = useState(null)
 	const [names, setNames] = useState({})
+	const [rates, setRates] = useState({})
 
 	useEffect( () => {
 		async function fetchData() {
-			// const currentRates = await axios.get('http://data.fixer.io/api/latest?access_key=b8d1c676f1ce61e37325aa06be69d48c')
-			// const { rates } = currentRates.data
+			const currentRates = await axios.get('http://data.fixer.io/api/latest?access_key=b8d1c676f1ce61e37325aa06be69d48c')
+			const { rates } = currentRates.data
+			setRates(rates)
 
-			// const currencies = await axios.get('http://data.fixer.io/api/symbols?access_key=b8d1c676f1ce61e37325aa06be69d48c')
-			// const { symbols } = currencies.data
-			// setNames(symbols)
+			const currencies = await axios.get('http://data.fixer.io/api/symbols?access_key=b8d1c676f1ce61e37325aa06be69d48c')
+			const { symbols } = currencies.data
+			setNames(symbols)
 		}
 		fetchData();
 	}, [])
 
 	const showInfo = (eventKey) => {
+		const abbr = Object.keys(names).find((key) => names[key] === eventKey)
+		const countryCodes = Object.keys(CountryCurrs).filter((key) => CountryCurrs[key] === abbr)
 
-		setCurrency({ name: eventKey, flag: 'https://flagcdn.com/16x12/za.png' })
+		let rate = rates[abbr]
+		const flags = countryCodes.map(code => code.toLowerCase())
+
+		setCurrency({ name: eventKey, flags, rate })
 	}
 
 	return (
@@ -31,13 +39,15 @@ const App = () => {
 			<Table>
 				<thead>
 					<tr>
-						<th>Flag</th>
-						<th>Name</th>
-						<th>Rate</th>
+						<th>Флаг</th>
+						<th>Название</th>
+						<th>Курс</th>
 					</tr>
 				</thead>
 				<tbody>
-					<CurrencyItem currency={currency} />
+				{
+					currency && <CurrencyItem currency={currency} />
+				}
 				</tbody>
 			</Table>
 		</Container>
--- src/components/CurrencyItem.js
+++ src/components/CurrencyItem.js
@@ -1,12 +1,30 @@
 import React from 'react'
+import codes from '../codes.json'
 
-const CurrencyItem = ({ currency: { flag, name, rate } }) => {
+const CurrencyItem = ({ currency: { flags, name, rate } }) => {
+
+    let selectedFlags = flags.map(flag => <img src={`https://flagcdn.com/16x12/${flag}.png`} alt='' />)
+    const firstCode = flags[0]
+    const firstFlag = selectedFlags[0]
+
+    selectedFlags.shift()
+    flags.shift()
+
     return (
-        <tr>
-            <td><img src={ flag } alt='' /></td>
-            <td> { name } </td>
-            <td> { rate } </td>
-        </tr>
+        <>
+            <tr>
+                <td>{ firstFlag } {codes[firstCode]}</td>
+                <td> { name } </td>
+                <td> { rate } </td>
+            </tr>
+            { selectedFlags
+                .map((flag, ind) =>
+                    <tr key={ind}>
+                        <td>{flag} {codes[flags[ind]]}</td>
+                    </tr>
+                )
+            }
+        </>
     );
 };
 
